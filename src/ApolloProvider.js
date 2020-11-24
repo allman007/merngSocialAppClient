a1506d8695dc5dcbeb0acfde743f416797ebe083
allman007 <nwanjiuche@gmail.com>
2020-11-24 08:30:33 +0100

    second

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from "@apollo/react-hooks";
 import { setContext } from "apollo-link-context";
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:5000",
+  uri: "https://merngsocialappserverside.herokuapp.com/",
 });
 
 // To eable the user post a comment, bcus witout Authorization d user cnt post
